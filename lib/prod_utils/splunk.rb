832aa5e4d00ee63496d77888b28ad975fb2d5251
Weston Dransfield <wdransfield@instructure.com>
2021-11-16 11:59:48 -0700

    Properly reference Database

--- lib/prod_utils/splunk.rb
+++ lib/prod_utils/splunk.rb
@@ -11,7 +11,7 @@ module ProdUtils::Splunk
       database_server_id: "cluster#{cluster}"
     ).limit(1).first.database_server.config[:region]
 
-    Database.slow_requests_for_hosts(
+    ProdUtils::Database.slow_requests_for_hosts(
       hosts_for_cluster(cluster),
       region: region_map[region]
     )
