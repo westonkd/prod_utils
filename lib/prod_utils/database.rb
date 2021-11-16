61c50c8c665cdccb94638f71e3332ac253178ff9
Weston Dransfield <wdransfield@instructure.com>
2021-11-16 12:14:33 -0700

    small refactor

--- lib/prod_utils/database.rb
+++ lib/prod_utils/database.rb
@@ -9,16 +9,26 @@ module ProdUtils::Database
     'ca-central-1' => 'yul',
   }
 
+  def self.region_for(shard)
+    shard.database_server.config[:region]
+  end
+
+  def self.hosts_for(shard)
+    hosts = []
+    shard.activate do
+      Account.root_accounts
+        .preload(:account_domains)
+        .each { |ra| ra.account_domains.each { |ad| hosts << ad.host } }
+    end
+    hosts.flatten
+  end
+
   def self.hosts_for_cluster(cluster)
     hosts = []
     Shard.where(
       database_server_id: "cluster#{cluster}"
     ).map do |shard|
-      shard.activate do
-        Account.root_accounts
-          .preload(:account_domains)
-          .each { |ra| ra.account_domains.each { |ad| hosts << ad.host } }
-      end
+      hosts << hosts_for(shard)
     end
     hosts
   end
--- lib/prod_utils/splunk.rb
+++ lib/prod_utils/splunk.rb
@@ -1,6 +1,10 @@
 module ProdUtils::Splunk
-  def self.slow_requests_for_hosts(hosts, region: 'iad')
-    query = "index=canvas_#{region} "
+  def self.base_query(region)
+    "index=canvas_#{ProdUtils::Database::REGION_MAP[region]} "
+  end
+
+  def self.slow_requests_for_hosts(hosts, region: 'us-east-1')
+    query = base_query(region)
     hosts.each_with_index { |host, i| query << "#{i == 0 ? '' : 'OR '}vhost=#{host} " }
     query << " | where microseconds < 5000000 | sort microseconds desc"
     query
@@ -13,7 +17,16 @@ module ProdUtils::Splunk
 
     slow_requests_for_hosts(
       ProdUtils::Database.hosts_for_cluster(cluster),
-      region: ProdUtils::Database::REGION_MAP[region]
+      region: region
+    )
+  end
+
+  def self.slow_requests_for_shard(shard_id)
+    shard = Shard.find shard_id
+
+    slow_requests_for_hosts(
+      ProdUtils::Database.hosts_for(shard),
+      ProdUtils::Database.region_for(shard)
     )
   end
 end
