cb7adcf33653fa8d4f2ed804a1c4a3389c6eef54
Weston Dransfield <wdransfield@instructure.com>
2022-07-27 12:55:50 -0600

    Add ll method for easy method identification

--- lib/prod_utils.rb
+++ lib/prod_utils.rb
@@ -3,4 +3,11 @@ require_relative "prod_utils/splunk"
 require_relative "prod_utils/database"
 
 module ProdUtils
+  def self.ll
+    [ProdUtils::Database, ProdUtils::Splunk].each do |mod|
+      puts "=== #{mod} ==="
+      puts mod.methods(false).join("\n")
+      puts ''
+    end
+  end
 end
